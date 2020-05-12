9ea3a62bbb52bba9e130c247099417dab367f21a
Fabrício Cunha <fabricio.cunha@projuris.com.br>
2020-05-11 21:12:11 -0300

    Fix go package in generated protobuf files

--- pkg/keeper/grpcserver.go
+++ pkg/keeper/grpcserver.go
@@ -12,6 +12,6 @@ type GRPCServer struct{}
 // Ask method for calculate X + Y
 func (s *GRPCServer) Ask(ctx context.Context, req *api.AskRequest) (*api.AskResponse, error) {
 	return &api.AskResponse{
-		Result: req.GetX() + req.GetY(),
+		AllowPass: false,
 	}, nil
 }
